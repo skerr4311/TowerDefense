79ac3ea85ff300ac38f79665368dca60ec056661
Steven Kerr <44153164+skerr4311@users.noreply.github.com>
2020-09-20 14:19:58 +1200

    I have an enemy object and I am moving it aloing the block path

---
+++ 5_Realm_Rush/Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<WayPoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintAllWaypoints());
+    }
+
+    IEnumerator PrintAllWaypoints()
+    {
+        print("Starting patrol...");
+        foreach(WayPoint wayPoint in path)
+        {
+            transform.position = wayPoint.transform.position;
+            print("Visiting " + wayPoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Finished patrol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
