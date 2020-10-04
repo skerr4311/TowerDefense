967e334f2e277ab80e5946d9b6274e1351fbaf4b
Steven Kerr <44153164+skerr4311@users.noreply.github.com>
2020-10-04 15:36:08 +1300

    Finished adding bullets to the tower and colliders to the enemy

---
+++ 5_Realm_Rush/Assets/EnemyDamage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        print("Current hitpoints are " + hitPoints);
+    }
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
